039541e025e6bdd688abaf778c1ad04090e5bf81
greynozo <kioc04@hotmail.com>
2025-04-16 16:42:54 -0300

    Clase 15/04/2025

---
+++ PracticaProfesional2025/Default.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Principal.Master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="PracticaProfesional2025.Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="p-4 p-md-5 pt-5">
+        <table class="table">
+            <tr>
+                <td>
+                    <asp:Label ID="lblUsuario" runat="server" Text="Usuario"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtUsuario" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="rfvTxtUsuario" runat="server" ErrorMessage="RequiredFieldValidator"
+                        Text="El campo usuario es obligatorio" ControlToValidate="txtUsuario"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblDNI" runat="server" Text="DNI"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtDNI" runat="server" TextMode="Number"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="rfvTxtDNI" runat="server" ErrorMessage="RequiredFieldValidator"
+                        Text="El campo dni es obligatorio" ControlToValidate="txtDNI"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblEdad" runat="server" Text="Edad"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtEdad" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="rfvTxtEdad" runat="server" ErrorMessage="RequiredFieldValidator"
+                        Text="El campo Edad es obligatorio" ControlToValidate="txtEdad"></asp:RequiredFieldValidator>
+                    <asp:RangeValidator ID="rvTxtEdad" runat="server" ErrorMessage="RangeValidator" ControlToValidate="txtEdad" Type="Integer" Text="Debe mayor de 18 años" MaximumValue="100" MinimumValue="18"></asp:RangeValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblContraseña" runat="server" Text="Contraseña"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtContraseña" runat="server" TextMode="Password"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="rfvTxtContraseña" runat="server" ErrorMessage="RequiredFieldValidator"
+                        Text="El campo contraseña es obligatorio" ControlToValidate="txtContraseña"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblRepetir" runat="server" Text="Contraseña"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtRepetir" runat="server" TextMode="Password"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="rfvRepetir" runat="server" ErrorMessage="RequiredFieldValidator"
+                        Text="El campo repetir es obligatorio" ControlToValidate="txtRepetir"></asp:RequiredFieldValidator>
+                    <asp:CompareValidator ID="cvRepetir" runat="server" ErrorMessage="CompareValidator"
+                        ControlToValidate="txtContraseña" ControlToCompare="txtRepetir" Text="Las contraseñas no coinciden"></asp:CompareValidator>
+                </td>
+            </tr>
+        </table>
+    </div>
+</asp:Content>
---
+++ PracticaProfesional2025/Default.aspx.designer.cs
@@ -0,0 +1,168 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace PracticaProfesional2025 {
+    
+    
+    public partial class Default {
+        
+        /// <summary>
+        /// Control lblUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsuario;
+        
+        /// <summary>
+        /// Control txtUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsuario;
+        
+        /// <summary>
+        /// Control rfvTxtUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvTxtUsuario;
+        
+        /// <summary>
+        /// Control lblDNI.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDNI;
+        
+        /// <summary>
+        /// Control txtDNI.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDNI;
+        
+        /// <summary>
+        /// Control rfvTxtDNI.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvTxtDNI;
+        
+        /// <summary>
+        /// Control lblEdad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEdad;
+        
+        /// <summary>
+        /// Control txtEdad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEdad;
+        
+        /// <summary>
+        /// Control rfvTxtEdad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvTxtEdad;
+        
+        /// <summary>
+        /// Control rvTxtEdad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator rvTxtEdad;
+        
+        /// <summary>
+        /// Control lblContraseña.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblContraseña;
+        
+        /// <summary>
+        /// Control txtContraseña.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtContraseña;
+        
+        /// <summary>
+        /// Control rfvTxtContraseña.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvTxtContraseña;
+        
+        /// <summary>
+        /// Control lblRepetir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblRepetir;
+        
+        /// <summary>
+        /// Control txtRepetir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRepetir;
+        
+        /// <summary>
+        /// Control rfvRepetir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvRepetir;
+        
+        /// <summary>
+        /// Control cvRepetir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator cvRepetir;
+    }
+}
